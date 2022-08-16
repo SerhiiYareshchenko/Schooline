8fced794c71e1be25237ac686c14ecb55ae4b3d2
Serhii_Yareshchenko <ipuzzle23@gmail.com>
2022-08-16 21:13:20 +0300

    chakra, react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from "./store";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
     <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>);
 
 /*interface State {
     store: Store,
